2bf66871352caf70dac3b78bfa35f1b861e42d0c
Greg Salivio <ssh://git@vcs.cdxlife.com:4141/>
2015-09-14 11:30:59 -0700

    add title with date

--- error.php
+++ error.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Edited today</title>
+<title>Edited today sept 15 2015</title>
 
 
 
